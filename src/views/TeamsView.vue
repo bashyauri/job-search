e1954c01ed9d11a62c4c377f8f5447ab0f052b26
bashyauri <80903696+bashyauri@users.noreply.github.com>
2022-07-13 23:52:39 +0100

    modified TeamsView

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,14 @@
 <template>
-  <header-container />
+  <header-container>
+    <template v-slot:title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+    <template v-slot:subtitle>
+      <h2 class="w-full my-4 text-base font-light">
+        It's awesome working here. Why dont you come join us?
+      </h2>
+    </template>
+  </header-container>
 </template>
 <script>
 import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
